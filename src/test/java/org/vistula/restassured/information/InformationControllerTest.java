26df94a5bf82a0913901b59d99b0e59d67135c16
slawekradzyminski <slawomir.radzyminski@gmail.com>
2019-11-24 16:48:30 +0100

    Zajecia 24.11

--- src/test/java/org/vistula/restassured/information/InformationControllerTest.java
+++ src/test/java/org/vistula/restassured/information/InformationControllerTest.java
@@ -1,10 +1,14 @@
 package org.vistula.restassured.information;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.vistula.restassured.RestAssuredTest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThan;
 
 public class InformationControllerTest extends RestAssuredTest {
 
@@ -17,4 +21,31 @@ public class InformationControllerTest extends RestAssuredTest {
                 .body("size()", is(2));
     }
 
+    @Test
+    public void shouldCreateNewPlayer() {
+        JSONObject requestParams = new JSONObject();
+        String myName = RandomStringUtils.randomAlphabetic(10);
+
+        requestParams.put("nationality", "Poland");
+        requestParams.put("name", myName);
+        requestParams.put("salary", 500);
+
+        given().header("Content-Type", "application/json")
+                .body(requestParams.toString())
+                .post("/information")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("nationality", equalTo("Poland"))
+                .body("salary", equalTo(500))
+                .body("name", equalTo(myName))
+                .body("id", greaterThan(0));
+    }
+
+    @Test
+    public void shouldDelete() {
+        given().delete("/information/6")
+                .then()
+                .log().all();
+    }
 }
--- src/test/java/org/vistula/restassured/pet/PetControllerTest.java
+++ src/test/java/org/vistula/restassured/pet/PetControllerTest.java
@@ -1,12 +1,15 @@
 package org.vistula.restassured.pet;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.vistula.restassured.RestAssuredTest;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 
 public class PetControllerTest extends RestAssuredTest {
 
@@ -28,12 +31,36 @@ public class PetControllerTest extends RestAssuredTest {
                 .body("id", is(1))
                 .body("name", equalTo("Cow"));
     }
+    @Test
+    public void shouldGetSecondPet() {
+        Pet pet = given().get("/pet/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().body().as(Pet.class);
+
+        assertThat(pet.getId()).isEqualTo(2);
+        assertThat(pet.getName()).isEqualTo("Dog");
+    }
+
+    @Test
+    public void shouldntGet100() {
+        String contentType = given().get("/pet/100")
+                .then()
+                .log().all()
+                .statusCode(404)
+                .body(equalTo("There is not Pet with such id"))
+                .extract().contentType();
+
+        assertThat(contentType).isEqualTo("text/plain;charset=UTF-8");
+    }
 
     @Test
     public void shouldCreateNewPet() {
         JSONObject requestParams = new JSONObject();
-        requestParams.put("id", 4);
-        requestParams.put("name", "Wolf");
+        int value = ThreadLocalRandom.current().nextInt(20, Integer.MAX_VALUE);
+        requestParams.put("id", value);
+        requestParams.put("name", RandomStringUtils.randomAlphabetic(10));
 
         given().header("Content-Type", "application/json")
                 .body(requestParams.toString())
@@ -41,6 +68,19 @@ public class PetControllerTest extends RestAssuredTest {
                 .then()
                 .log().all()
                 .statusCode(201);
+
+        given().delete("/pet/"+value)
+                .then()
+                .log().all()
+                .statusCode(204);
     }
 
+    @Test
+    public void shouldDeletePet() {
+        given().delete("/pet/4")
+                .then()
+                .log().all()
+                .statusCode(204);
+
+    }
 }
